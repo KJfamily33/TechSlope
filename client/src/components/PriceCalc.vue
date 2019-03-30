c0bb2d9946b2163ca0af1c1580969e1eb53906a2
mdf96 <me@marcdfiot.com>
2019-03-30 16:38:31 -0600

    Added cost estimate

--- client/src/components/PriceCalc.vue
+++ client/src/components/PriceCalc.vue
@@ -1,8 +1,9 @@
 <template>
 
   <v-flex xs12>
+    <div class="display-3"> Estimated Cost: ${{ cost }}</div>
     <v-subheader class="pl-0">API Calls per Day</v-subheader>
-    <v-slider v-model="slider"
+    <v-slider v-model="calls"
     min="0"
     max="9999"
     thumb-label="always"
@@ -17,7 +18,12 @@
   export default {
     name: "priceCalc",
     data: () => ({
-      slider: 2000,
+      calls: 812,
     }),
+    computed: {
+      cost: function () {
+        return (this.calls*.007).toFixed(2)
+      }
+    }
   }
 </script>
